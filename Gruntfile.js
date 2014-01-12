a7a8c2c5fc1a054d3d0e86639b31e2b604c5238c
Fabrice Weinberg <Fabrice@weinberg.me>
2014-01-12 17:58:00 +0100

    Build concat.dist.src dynamically from src/matrix.scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,33 +15,7 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n',
       },
       dist: {
-        src: [
-          'src/functions/helpers/_valid-coords.scss',
-          'src/functions/init/_matrix.scss',
-          'src/functions/init/_unit-matrix.scss',
-          'src/functions/misc/_rows.scss',
-          'src/functions/misc/_columns.scss',
-          'src/functions/setters/_set-entry.scss',
-          'src/functions/setters/_set-row.scss',
-          'src/functions/setters/_set-column.scss',
-          'src/functions/getters/_get-entry.scss',
-          'src/functions/getters/_get-row.scss',
-          'src/functions/getters/_get-column.scss',
-          'src/functions/misc/_display.scss',
-          'src/functions/swap/_swap-entries.scss',
-          'src/functions/swap/_swap-rows.scss',
-          'src/functions/swap/_swap-columns.scss',
-          'src/functions/checkers/_is-numeric.scss',
-          'src/functions/checkers/_is-square.scss',
-          'src/functions/checkers/_is-diagonal.scss',
-          'src/functions/helpers/_is-triangular.scss',
-          'src/functions/checkers/_is-upper-triangular.scss',
-          'src/functions/checkers/_is-lower-triangular.scss',
-          'src/functions/misc/_add-matrices.scss',
-          'src/functions/misc/_transpose.scss',
-          'src/functions/setters/_add-row.scss',
-          'src/functions/setters/_add-colum.scssn'
-        ],
+        // src: Will be filed by `updateConcatFiles
         dest: 'dist/_SassyMatrix.scss',
       },
     },
@@ -84,5 +58,23 @@ module.exports = function(grunt) {
   // Tasks
   grunt.registerTask('test', ['sass', 'bootcamp']);
   grunt.registerTask('dev', ['test', 'watch']);
-  grunt.registerTask('build', ['test', 'concat']);
+  grunt.registerTask('build', ['updateConcatFiles', 'test', 'concat']);
+
+  grunt.registerTask('updateConcatFiles', 'pull out the files form `src/matrix.scss` ', function() {
+    var matricContent = grunt.file.read('src/matrix.scss'),
+        regex = /@import\s*"(.*?)";/g,
+        paths = [],
+        match;
+
+    while ( ( match = regex.exec(matricContent)) !== null ){
+      var path = match[1],
+          realPath = path.substr(0, path.lastIndexOf('/')+1),
+          fileName = path.substr(path.lastIndexOf('/')+1);
+
+      paths.push ( 'src/' + realPath + '_' + fileName + '.scss' );
+    }
+
+    grunt.config.set('concat.dist.src', paths);
+  });
+
 };
