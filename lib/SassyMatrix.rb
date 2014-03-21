2c084c619d8f3413a045b52cb9998d9fdbdb7388
Jed Foster <jed@jedfoster.com>
2014-03-21 15:57:50 -0700

    Correct extension name
    Was previously being registered with Compass as "SassyJSON", which breaks things if you are using SassyJSON.

--- lib/SassyMatrix.rb
+++ lib/SassyMatrix.rb
@@ -1,12 +1,12 @@
 require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('SassyJSON', :path => extension_path)
+Compass::Frameworks.register('SassyMatrix', :path => extension_path)
 
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyMatrix
-  VERSION = "1.0.0"
-  DATE = "2014-01-23"
+  VERSION = "1.0.1"
+  DATE = "2014-03-21"
 end
 
 module Sass::Script::Functions
